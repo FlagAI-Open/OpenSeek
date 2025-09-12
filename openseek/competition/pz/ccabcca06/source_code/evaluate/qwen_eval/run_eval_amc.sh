43627d2428ad74c78cf6c64e5e439b7c4344e703
ccabcca06 <281150668@qq.com>
2025-09-12 21:15:11 +0800

    提交说明：添加ccabcca06的初赛报告、决赛报告和代码

---
+++ openseek/competition/pz/ccabcca06/source_code/evaluate/qwen_eval/run_eval_amc.sh
@@ -0,0 +1,32 @@
+set -ex
+
+PROMPT_TYPE="prompt_amc"
+MODEL_NAME_OR_PATH=$1
+MODEL_NAME_SHORT=$(basename $1)
+OUTPUT_DIR="eval_final"
+
+SPLIT="test"
+NUM_TEST_SAMPLE=-1
+
+DATA_NAME="amc23"
+TOKENIZERS_PARALLELISM=false \
+
+python3 -u math_eval.py \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --data_name ${DATA_NAME} \
+    --output_dir ${OUTPUT_DIR} \
+    --split ${SPLIT} \
+    --prompt_type ${PROMPT_TYPE} \
+    --num_test_sample ${NUM_TEST_SAMPLE} \
+    --seed 0 \
+    --temperature 0.05 \
+    --n_sampling 16 \
+    --max_tokens_per_call 2784 \
+    --top_p 1 \
+    --start 0 \
+    --end -1\
+    --use_sglang \
+    --pipeline_parallel_size 1 \
+    --dp_size 1 \
+    --save_outputs \
+    --overwrite \
